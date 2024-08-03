9eba7c443c0f2fccf3ad5efa4ba37e338749a1b2
Connor <kor.connor@gmail.com>
2024-08-03 11:01:51 +0900

    feat: 온보딩, 마이페이지 sample api

--- src/my/my.module.ts
+++ src/my/my.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common'
 import { MyService } from './my.service'
 import { MyController } from './my.controller'
+import { MongooseModule } from '@nestjs/mongoose'
+import { Cat, CatSchema } from 'src/schemas/my.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [MyController],
   providers: [MyService],
 })
